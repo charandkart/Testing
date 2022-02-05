07a1e0c5cfadfcfab9789e9fcbb20e6afe0bc596
charan <charan.kr@dolphinskart.com>
2022-02-06 00:20:21 +0530

    updated index.html

--- src/Pages/ArticleList.js
+++ src/Pages/ArticleList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ArticleList = () => {
   return (
     <div>
       <h1> This is Article List File</h1>
+      {[1, 2, 3].map((i) => (
+        <div>
+          <Link to={`/article/${i}`}>Article - {i}</Link>
+        </div>
+      ))}
     </div>
   );
 };
