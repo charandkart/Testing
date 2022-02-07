94b57607683c72d6924b59485947a9518f5baf4a
hemanthcsdkart <hemanthcs@dkart.com>
2022-02-07 12:45:48 +0530

    adding Help

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, NavLink, Link } from "react-router-dom";
 import "./App.css";
 import Article from "./Pages/Article";
 import ArticleList from "./Pages/ArticleList";
+import Help from "./Pages/Help";
 import Home from "./Pages/Home";
 
 function App() {
@@ -11,12 +12,14 @@ function App() {
         <div>
           <Link to="/">Home</Link>
           <Link to="/articles">Articles</Link>
+          <Link to="/help">Help</Link>
         </div>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route exact path="/articles" element={<ArticleList />} />
         <Route path="/article/:slug" element={<Article />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </>
   );
