2861c26cebc39896f0618e31de377313464f95bd
hemanthcsdkart <hemanthcs@dkart.com>
2022-02-07 17:35:21 +0530

    changes back in home route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Article from "./Pages/Article";
 import ArticleList from "./Pages/ArticleList";
 import Help from "./Pages/Help";
-
 import Home from "./Pages/Home";
 
 function App() {
@@ -13,14 +12,14 @@ function App() {
         <div>
           <Link to="/articles">Articles</Link>
           <Link to="/help">Help</Link>
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
         </div>
       </header>
       <Routes>
         <Route exact path="/articles" element={<ArticleList />} />
         <Route path="/article/:slug" element={<Article />} />
         <Route path="/help" element={<Help />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
