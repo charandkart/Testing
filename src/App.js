c6f4e9319a4796651112f3ef126c3ee391a00fe1
hemanthcsdkart <hemanthcs@dkart.com>
2022-02-09 19:15:34 +0530

    modifies back in app filess

--- src/App.js
+++ src/App.js
@@ -5,32 +5,24 @@ import Article from "./Pages/Article";
 import ArticleList from "./Pages/ArticleList";
 import Help from "./Pages/Help";
 import Home from "./Pages/Home";
-import Helmet from "react-helmet";
 
 function App() {
   return (
     <>
-      <Helmet>
-        <title>Entertainment Hub</title>
-        <meta
-          name="description"
-          content="Get info for all favorite movies and stories"
-        />
-        <meta name="keyword" content="Movies,Tv Series,Search Movies,News" />
-      </Helmet>
+      <header>
+        <div>
+          <Link to="/articles">Articles</Link>
+          <Link to="/help">Help</Link>
+          <Link to="/">Home</Link>
+        </div>
+      </header>
+      <Routes>
+        <Route path="/help" element={<Help />} />
+        <Route path="/" element={<Home />} />
 
-      <div>
-        <Link to="/articles">Articles</Link>
-        <Link to="/help">Help</Link>
-        <Link to="/home">Home</Link>
-
-        <Routes>
-          <Route exact path="/articles" element={<ArticleList />} />
-          <Route path="/article/:slug" element={<Article />} />
-          <Route path="/help" component={Help} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
+        <Route path="/article/:slug" element={<Article />} />
+        <Route exact path="/articles" element={<ArticleList />} />
+      </Routes>
     </>
   );
 }
