b3cc2b0b2e33f10aaf452abd70889cc222a3f8bf
hemanthcsdkart <hemanthcs@dkart.com>
2022-02-09 17:31:30 +0530

    changes in Helmets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Routes, Route, NavLink, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About";
 import Article from "./Pages/Article";
 import ArticleList from "./Pages/ArticleList";
 import Help from "./Pages/Help";
 import Home from "./Pages/Home";
+import Helmet from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Entertainment Hub</title>
+        <meta
+          name="description"
+          content="Get info for all favorite movies and stories"
+        />
+        <meta name="keyword" content="Movies,Tv Series,Search Movies,News" />
+      </Helmet>
       <header>
         <div>
           <Link to="/articles">Articles</Link>
--- src/Pages/Help.js
+++ src/Pages/Help.js
@@ -1,13 +1,16 @@
 import React from "react";
+
 //import { Link } from "react-router-dom";
 import Meta from "../Components/Meta";
 
 const Help = () => {
   return (
-    <div>
-      <Meta title="Help page" description="Help page Description" />
-      <h1> This is Help List File</h1>
-    </div>
+    <>
+      <div>
+        <Meta title="Help page" description="Help page Description" />
+        <h1> This is Help List File</h1>
+      </div>
+    </>
   );
 };
 
