0ca112f70fa520752145c9b46ebcc71973df1773
hemanthcsdkart <hemanthcs@dkart.com>
2022-02-09 17:32:12 +0530

    add aboutjs files

---
+++ src/Pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import AboutPage from "./AboutPage";
+
+const About = () => {
+  return (
+    <div>
+      <>
+        <Link to="/aboutPage">AboutPage</Link>
+      </>
+      <Route to="/aboutPage" component={AboutPage} />
+      <h1>AboutPage</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+import Meta from "../Components/Meta";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Meta title="About page" description="About page Description" />
+      <h1>AboutPage Components</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
